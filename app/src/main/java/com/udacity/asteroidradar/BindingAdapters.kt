4656485a668309c90c96bb6cbd97ea249151a3c9
Romi Agar <3225967+r0mi@users.noreply.github.com>
2021-03-12 17:04:06 +0200

    Add additional binding adapters

--- app/src/main/java/com/udacity/asteroidradar/BindingAdapters.kt
+++ app/src/main/java/com/udacity/asteroidradar/BindingAdapters.kt
@@ -1,8 +1,13 @@
 package com.udacity.asteroidradar
 
+import android.view.View
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.udacity.asteroidradar.main.AsteroidListAdapter
+import com.udacity.asteroidradar.repository.AsteroidRadarApiStatus
 
 @BindingAdapter("statusIcon")
 fun bindAsteroidStatusImage(imageView: ImageView, isHazardous: Boolean) {
@@ -31,6 +36,22 @@ fun bindDetailsStatusImage(imageView: ImageView, isHazardous: Boolean) {
     }
 }
 
+@BindingAdapter("pictureOfDayImage")
+fun bindPictureOfDayImage(imageView: ImageView, pictureOfDay: PictureOfDay?) {
+    if (pictureOfDay != null) {
+        imageView.loadImage(pictureOfDay.url)
+    }
+}
+
+@BindingAdapter("pictureOfDayImageContentDescription")
+fun bindPictureOfDayImageContentDescription(imageView: ImageView, pictureOfDay: PictureOfDay?) {
+    if (pictureOfDay != null) {
+        imageView.contentDescription = imageView.context.getString(R.string.nasa_picture_of_day_content_description_format, pictureOfDay.title)
+    } else {
+        imageView.contentDescription = imageView.context.getString(R.string.this_is_nasa_s_picture_of_day_showing_nothing_yet)
+    }
+}
+
 @BindingAdapter("astronomicalUnitText")
 fun bindTextViewToAstronomicalUnit(textView: TextView, number: Double) {
     val context = textView.context
@@ -47,4 +68,25 @@ fun bindTextViewToKmUnit(textView: TextView, number: Double) {
 fun bindTextViewToDisplayVelocity(textView: TextView, number: Double) {
     val context = textView.context
     textView.text = String.format(context.getString(R.string.km_s_unit_format), number)
+}
+
+@BindingAdapter("listData")
+fun RecyclerView.bindRecyclerView(data: List<Asteroid>?) {
+    val adapter = adapter as AsteroidListAdapter
+    adapter.submitList(data)
+}
+
+@BindingAdapter("asteroidApiStatus")
+fun ProgressBar.bindStatus(status: AsteroidRadarApiStatus?) {
+    when (status) {
+        AsteroidRadarApiStatus.LOADING -> {
+            visibility = View.VISIBLE
+        }
+        AsteroidRadarApiStatus.ERROR -> {
+            visibility = View.GONE
+        }
+        AsteroidRadarApiStatus.DONE -> {
+            visibility = View.GONE
+        }
+    }
 }
