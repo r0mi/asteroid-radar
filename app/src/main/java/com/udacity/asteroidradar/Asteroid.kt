1136e806e503359583662330ad331c326c7e8f6a
Romi Agar <3225967+r0mi@users.noreply.github.com>
2021-03-12 17:03:07 +0200

    Use extension function to convert date

--- app/src/main/java/com/udacity/asteroidradar/Asteroid.kt
+++ app/src/main/java/com/udacity/asteroidradar/Asteroid.kt
@@ -4,25 +4,21 @@ import android.os.Parcelable
 import com.udacity.asteroidradar.database.DatabaseAsteroid
 import kotlinx.parcelize.Parcelize
 import java.text.ParseException
-import java.text.SimpleDateFormat
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
 import java.util.*
 
 @Parcelize
-data class Asteroid(val id: Long, val codename: String, val closeApproachDate: String,
-                    val absoluteMagnitude: Double, val estimatedDiameter: Double,
-                    val relativeVelocity: Double, val distanceFromEarth: Double,
-                    val isPotentiallyHazardous: Boolean) : Parcelable
+data class Asteroid(
+    val id: Long, val codename: String, val closeApproachDate: String,
+    val absoluteMagnitude: Double, val estimatedDiameter: Double,
+    val relativeVelocity: Double, val distanceFromEarth: Double,
+    val isPotentiallyHazardous: Boolean
+) : Parcelable
 
 fun List<Asteroid>.asDatabaseModel(): Array<DatabaseAsteroid> {
     return map {
         val calendar = Calendar.getInstance()
         val date = try {
-            SimpleDateFormat(
-                Constants.API_QUERY_DATE_FORMAT,
-                Locale.getDefault()
-            ).parse(it.closeApproachDate) ?: calendar.time
+            it.closeApproachDate.toDate(Constants.API_QUERY_DATE_FORMAT) ?: calendar.time
         } catch (e: ParseException) {
             calendar.time
         }
